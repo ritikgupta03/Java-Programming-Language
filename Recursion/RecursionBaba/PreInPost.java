2be23024278bd97f3672d7599c5b2391900360af
ritikgupta03 <ritikgupta3089@gmail.com>
2026-07-13 00:27:40 +0530

    Practice DSA

--- Recursion/RecursionBaba/PreInPost.java
+++ Recursion/RecursionBaba/PreInPost.java
@@ -7,13 +7,14 @@ public class PreInPost {
 
     public static void pip(int n) {
         if(n==0) return;
-        System.out.print(n+" ");
+        System.out.print(n+" "); //pre
         pip(n-1);
-        System.out.print(n+" ");
+        System.out.print(n+" "); //In
         pip(n-1);
-        System.out.print(n+" ");
+        System.out.print(n+" "); //post
     }
 }
+
 //n=1----------output is= 1 1 1
 //n=2----------output is= 2 1 1 1 2 1 1 1 2
 //n=3----------output is= 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
