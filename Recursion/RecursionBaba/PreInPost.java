6c9fbff2a54b18af23e0e3afb83163818d7e4e91
ritikgupta03 <ritikgupta3089@gmail.com>
2026-07-12 22:40:27 +0530

    Recursion/RecursionBaba/PreInPost.java

---
+++ Recursion/RecursionBaba/PreInPost.java
@@ -0,0 +1,19 @@
+package Recursion.RecursionBaba;
+
+public class PreInPost {
+    public static void main(String[] args) {
+        pip(3);
+    }
+
+    public static void pip(int n) {
+        if(n==0) return;
+        System.out.print(n+" ");
+        pip(n-1);
+        System.out.print(n+" ");
+        pip(n-1);
+        System.out.print(n+" ");
+    }
+}
+//n=1----------output is= 1 1 1
+//n=2----------output is= 2 1 1 1 2 1 1 1 2
+//n=3----------output is= 3 2 1 1 1 2 1 1 1 2 3 2 1 1 1 2 1 1 1 2 3
