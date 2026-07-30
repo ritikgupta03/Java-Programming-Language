8599c479af0ec638fe2a80ff73ea5c2d99cd959a
ritikgupta03 <ritikgupta3089@gmail.com>
2026-07-31 00:22:28 +0530

    Slove the Array List approch

--- Array_List/AlternateArray.java
+++ Array_List/AlternateArray.java
@@ -1,4 +1,16 @@
 package Array_List;
 
+import java.util.ArrayList;
+
 public class AlternateArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        ArrayList<Integer> ans=new ArrayList<>();
+        for(int i=0;i<arr.length-1;i+=2){
+            ans.add(arr[i]);
+
+        }
+        System.out.println(ans);
+
+    }
 }
