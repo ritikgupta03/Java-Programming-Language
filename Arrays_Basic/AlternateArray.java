c254a77d1d76139d4b954f1099742849e8c704c2
ritikgupta03 <ritikgupta3089@gmail.com>
2026-07-31 00:21:09 +0530

    Arrays Print in Alternate way

--- Arrays_Basic/AlternateArray.java
+++ Arrays_Basic/AlternateArray.java
@@ -1,4 +1,15 @@
 package Arrays_Basic;
 
 public class AlternateArray {
+    public static void main(String args[]){
+        int[] arr={1,2,3,4,5,6};
+        int ans=0;
+        for(int i=0;i<arr.length-1;i+=2){
+            ans=arr[i];
+            System.out.print(ans+" ");
+        }
+
+
+    }
+
 }
