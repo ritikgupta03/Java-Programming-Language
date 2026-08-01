8aaaca08c9c22c74c48ec7a03e508a7cd2d5c9cb
ritikgupta03 <ritikgupta3089@gmail.com>
2026-08-01 23:28:58 +0530

    Initial commit - Java DSA practice

--- Arrays_Basic/MissingNumber.java
+++ Arrays_Basic/MissingNumber.java
@@ -1,15 +1,28 @@
 package Arrays_Basic;
-
+import java.util.*;
 public class MissingNumber {
     public static void main(String[] args) {
         int[] arr={0,1,2,3,5};
         int n=arr.length;
-        int sum=n*(n+1)/2;
-        int arrSum=0;
+   //-------------------- method--1------------------------------------
+//        int sum=n*(n+1)/2;
+//        int arrSum=0;
+//        for(int i=0;i<n;i++){
+//            arrSum+=arr[i];
+//        }
+//        int missingnumber=sum-arrSum;
+//        System.out.print(missingnumber);
+
+
+        //-------------------- method--2------------------------------------
+        Arrays.sort(arr);
         for(int i=0;i<n;i++){
-            arrSum+=arr[i];
+            if(arr[i]!=i){
+                System.out.print(i);
+                return;
+            }
         }
-        int missingnumber=sum-arrSum;
-        System.out.print(missingnumber);
+        //if not found---
+        System.out.print(arr.length);
     }
 }
